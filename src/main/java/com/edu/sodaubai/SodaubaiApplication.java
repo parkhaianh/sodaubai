10ab50488309d8caa0ae171ee3dd8c2905f410f8
Hai Anh Truong <anh.truong@upraxis.com>
2019-11-12 01:02:17 +0700

    re init

---
+++ src/main/java/com/edu/sodaubai/SodaubaiApplication.java
@@ -0,0 +1,13 @@
+package com.edu.sodaubai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SodaubaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SodaubaiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/edu/sodaubai/SodaubaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edu.sodaubai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SodaubaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
