eac3de8c1857b075ed67b8d17e7773804f53b0fa
Hai Anh Truong <anh.truong@upraxis.com>
2019-11-12 00:03:27 +0700

    init project

---
+++ src/main/java/haianh/com/edu/sodaubai/SodaubaiApplication.java
@@ -0,0 +1,13 @@
+package haianh.com.edu.sodaubai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SodaubaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SodaubaiApplication.class, args);
+	}
+
+}
